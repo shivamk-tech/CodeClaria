a80cadb2b2e6be5c9d86e8cfd48c5643ee11cfa3
shivamk-tech <skkumarsv786@gmail.com>
2026-04-03 06:44:28 +0530

    updated

--- codeclaria/src/app/page.tsx
+++ codeclaria/src/app/page.tsx
@@ -1,23 +1,27 @@
 import Hero from '../components/Hero';
 import FloatingLines from '../components/FloatingLines';
+import Features from '../components/Features';
 
 export default function Home() {
   return (
-    <div className="relative h-screen flex items-center justify-center">
-      <div className="absolute inset-0 z-0 h-screen">
-        <FloatingLines
-          enabledWaves={["top", "middle", "bottom"]}
-          lineCount={10}
-          lineDistance={5}
-          bendRadius={5}
-          bendStrength={-0.5}
-          interactive={true}
-          parallax={true}
-        />
+    <div>
+      <div className="relative h-screen flex items-center justify-center">
+        <div className="absolute inset-0 z-0 h-screen">
+          <FloatingLines
+            enabledWaves={["top", "middle", "bottom"]}
+            lineCount={10}
+            lineDistance={5}
+            bendRadius={5}
+            bendStrength={-0.5}
+            interactive={true}
+            parallax={true}
+          />
+        </div>
+        <div className="relative z-10 -mt-16">
+          <Hero />
+        </div>
       </div>
-      <div className="relative z-10 ">
-        <Hero />
-      </div>
+      <Features />
     </div>
   );
 }
